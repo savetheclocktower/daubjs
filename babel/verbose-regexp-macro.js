b96dbe8a787ebc74120fb3d5e1a988af4e2a2791
Andrew Dupont <github@andrewdupont.net>
2019-09-06 23:30:26 -0500

    Clean up the macro.

--- babel/verbose-regexp-macro.js
+++ babel/verbose-regexp-macro.js
@@ -1,5 +1,4 @@
 /* eslint-env node */
-// import { VerboseRegExp } from '../src/utils';
 
 const { VerboseRegExp } = require('../src/utils/verbose-regexp');
 
@@ -9,46 +8,6 @@ const { VerboseRegExp } = require('../src/utils/verbose-regexp');
 //
 // If it ever stops working for whatever reason, I can just disable it and
 // fall back to runtime behavior.
-
-function _isEscapedHash (line, index) {
-  return (index === 0) ? false : line.charAt(index - 1) === '\\';
-}
-
-function _trimCommentsFromLine (line) {
-  let hashIndex = -1;
-  do { hashIndex = line.indexOf('#', hashIndex + 1); }
-  while ( hashIndex > -1 && _isEscapedHash(line, hashIndex) );
-
-  if (hashIndex > -1) { line = line.substring(0, hashIndex); }
-  line = line.trim();
-  return line;
-}
-
-// A tagged template literal that allows you to define a verbose regular
-// expression using backticks. Literal whitespace is ignored, and you can use
-// `#` to mark comments. This makes long regular expressions way easier for
-// humans to read and write.
-//
-// Escape sequences _do not_ need to be double-escaped, with one exception:
-// capture group backreferences like \5 need to be written as \\5, because JS
-// doesn't understand that syntax outside of a literal RegExp.
-// function VerboseRegExp (str, ...args) {
-//   let raw = String.raw(str, ...args);
-//
-//   let pattern = raw.split(/\n/)
-//     .map(_trimCommentsFromLine)
-//     .join('')
-//     .replace(/\s/g, '');
-//
-//   // Take (e.g.) `\\5` and turn it into `\5`. For some reason we can't do
-//   // this with raw strings.
-//   pattern = pattern.replace(/(\\)(\\)(\d+)/g, (m, _, bs, d) => {
-//     return `${bs}${d}`;
-//   });
-//
-//   let result = new RegExp(pattern);
-//   return result;
-// }
 
 function isOurs (path) {
   if (!path || !path.node || !path.node.tag) { return false; }
@@ -61,18 +20,20 @@ function verboseRegExpPlugin (babel) {
     visitor: {
       TaggedTemplateExpression (path, state) {
         if (!isOurs(path)) { return; }
-        _handleReference(path, state, babel);
+        handleReference(path, state, babel);
       }
     }
   };
 }
 
-function _handleReference (path, state, babel) {
+function handleReference (path, state, babel) {
   let { types } = babel;
   let { quasi } = path.node;
 
   if (quasi.expressions && quasi.expressions.length > 0) {
-    throw new Error('VerboseRegExp macro doesn’t work with interpolations!');
+    // We can't pre-compile any template strings that contain interpolations.
+    // Return and let them get handled at runtime.
+    return;
   }
 
   let escapedTemplateString = quasi.quasis[0].value.raw;
@@ -83,14 +44,11 @@ function _handleReference (path, state, babel) {
   let pattern = VerboseRegExp(str);
 
   // `RegExpLiteral` expects a string.
-  pattern = pattern.toString();
-  pattern = pattern.slice(1, -1);
+  pattern = pattern.toString().slice(1, -1);
 
-  let quasiPath = path.get('quasi');
-  quasiPath.parentPath.replaceWith(
+  path.get('quasi').parentPath.replaceWith(
     types.RegExpLiteral(pattern)
   );
 }
 
 module.exports = verboseRegExpPlugin;
-// export default ;
