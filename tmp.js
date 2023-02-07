700d44db9b8e2d19371061750c25ed9abdf7e8f8
Andrew Dupont <github@andrewdupont.net>
2023-02-06 21:42:42 -0800

    Clean up stray files.

--- tmp.js
+++
@@ -1,8 +0,0 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import * as Utils from './utils';
-import Context from './context';
-import Grammar from './grammar';
-import Highlighter from './highlighter';
-import Lexer from './lexer';
-export { Context, Grammar, Highlighter, Lexer, Utils };
