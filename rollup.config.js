118237ad19f69ab2107728fc0727bf63ffb88596
Andrew Dupont <github@andrewdupont.net>
2023-02-08 22:33:10 -0800

    Add built `lib` directory.

--- rollup.config.js
+++ rollup.config.js
@@ -7,9 +7,24 @@ import babel from '@rollup/plugin-babel';
 import strip from '@rollup/plugin-strip';
 import terser from '@rollup/plugin-terser';
 
+import glob from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import PACKAGE from './package.json';
 
 let extraPlugins = [];
+
+const mappings = Object.fromEntries(
+  glob.sync('src/**/*.js').map(file => {
+    let from = path.relative(
+      'src',
+      file.slice(0, file.length - path.extname(file).length)
+    );
+    let to = fileURLToPath(new URL(file, import.meta.url));
+    return [from, to];
+  })
+);
 
 if (!process.env.DEBUG) {
   extraPlugins.push(
@@ -56,6 +71,23 @@ export default [
         // Only transpile our source code.
         exclude: 'node_modules/**',
       }),
+      ...extraPlugins
+    ]
+  },
+
+  // ES6 converted to logging-stripped ES6.
+
+  // “If you want to convert a set of files to another format while maintaining
+  // the file structure and export signatures, the recommended way … is to turn
+  // every file into an entry point. You can do so dynamically e.g. via the
+  // glob package.”
+  {
+    input: mappings,
+    output: {
+      dir: 'lib'
+    },
+    plugins: [
+      resolve({ exportConditions: ['node'] }),
       ...extraPlugins
     ]
   },
